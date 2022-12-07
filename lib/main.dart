66d8df1609e2bbc42316b0c264a946693b016f68
sqrmyy <dn41.ra20adtdjw@gmail.com>
2022-12-08 08:35:26 +0900

    動くアプリ

--- lib/main.dart
+++ lib/main.dart
@@ -1,81 +1,62 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            CircleAvatar(
-              radius: 50.0,
-              backgroundColor: Colors.white,
-              backgroundImage: AssetImage("images/face.png"),
-            ),
-            Text(
-              "O-matsu",
-              style: TextStyle(
-                fontFamily: "Pacifico",
-                fontSize: 40.0,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-              ),
-            ),
-            Text(
-              "FLUTTER DEVELOPER",
-              // style: TextStyle(),
-            ),
-            SizedBox(
-              height: 30.0,
-              width: 150.0,
-              child: Divider(
-                color: Colors.teal[100],
-              ),
-            ),
-            Card(
-              margin: EdgeInsets.symmetric(
-                vertical: 10,
-                horizontal: 50,
-              ),
-              child: ListTile(
-                leading: Icon(
-                  Icons.phone,
-                  color: Colors.teal,
-                ),
-                title: Text(
-                  "+81 999 999",
-                  style: TextStyle(
-                    fontFamily: "SourceSansPro",
-                    fontSize: 20.0,
-                    color: Colors.teal[900],
-                  ),
-                ),
-              ),
-            ),
-            Card(
-              margin: EdgeInsets.symmetric(
-                vertical: 10,
-                horizontal: 50,
-              ),
-              child: ListTile(
-                leading: Icon(
-                  Icons.email,
-                  color: Colors.teal,
-                ),
-                title: Text(
-                  "o-matsu@gmail.com",
-                  style: TextStyle(
-                    fontFamily: "SourceSansPro",
-                    fontSize: 20.0,
-                    color: Colors.teal[900],
-                  ),
-                ),
-              ),
-            ),
-          ],
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.red,
         ),
+        body: DicePage(),
       ),
     ),
   );
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  var leftDiceNumber = 1;
+  var rightDiceNumber = 1;
+
+  void changeDiceFace() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                changeDiceFace();
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                changeDiceFace();
+              },
+              child: Image.asset('images/dice$rightDiceNumber.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
