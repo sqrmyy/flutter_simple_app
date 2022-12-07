69f4c8845d271c6b3b917bc5c7f115ca26a605ff
sqrmyy <dn41.ra20adtdjw@gmail.com>
2022-12-07 20:50:08 +0900

    画面レイアウト

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,76 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('I am rich'),
-        ),
-        body: Center(
-          child: Text(
-            'Hello World!',
-          ),
+        backgroundColor: Colors.teal,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 50.0,
+              backgroundColor: Colors.white,
+              backgroundImage: AssetImage("images/face.png"),
+            ),
+            Text(
+              "O-matsu",
+              style: TextStyle(
+                fontFamily: "Pacifico",
+                fontSize: 40.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            Text(
+              "FLUTTER DEVELOPER",
+              // style: TextStyle(),
+            ),
+            SizedBox(
+              height: 30.0,
+              width: 150.0,
+              child: Divider(
+                color: Colors.teal[100],
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(
+                vertical: 10,
+                horizontal: 50,
+              ),
+              child: ListTile(
+                leading: Icon(
+                  Icons.phone,
+                  color: Colors.teal,
+                ),
+                title: Text(
+                  "+81 999 999",
+                  style: TextStyle(
+                    fontFamily: "SourceSansPro",
+                    fontSize: 20.0,
+                    color: Colors.teal[900],
+                  ),
+                ),
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(
+                vertical: 10,
+                horizontal: 50,
+              ),
+              child: ListTile(
+                leading: Icon(
+                  Icons.email,
+                  color: Colors.teal,
+                ),
+                title: Text(
+                  "o-matsu@gmail.com",
+                  style: TextStyle(
+                    fontFamily: "SourceSansPro",
+                    fontSize: 20.0,
+                    color: Colors.teal[900],
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     ),
